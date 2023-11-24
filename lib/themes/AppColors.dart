cb05e2b59ba7bcbd986b370aaa990e23c493e2cb
Slava <plumsoftwareofficial@gmail.com>
2023-11-24 14:48:58 +0600

    [+] Added app colors file with in app colors

---
+++ lib/themes/AppColors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color seed = Color.fromARGB(1, 8, 166, 82);
+
+  //region::Widgets
+  static const Color mainBackground = Color(0xFFFAFAFA);
+  static const Color chipBackground = Color(0x14000000);
+  static const Color tabBarIndicator = Color(0xFF068441);
+  static const Color tabBarBackground = Colors.white;
+  //endregion
+
+  //region::Text
+  static const Color title = Colors.black;
+  static const Color subtitle = Color(0x8C000000);
+  //endregion
+}
